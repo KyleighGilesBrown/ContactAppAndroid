e388c38641adc491af4fd5f80b21d71bea8bd034
kyleighgiles-brown <kyleighgilesbrown@gmail.com>
2026-02-02 15:26:07 -0500

    dbhelper

--- app/src/main/java/ContactDBHelper.java
+++ app/src/main/java/ContactDBHelper.java
@@ -1,2 +1,34 @@
-public class ContactDBHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class ContactDBHelper extends SQLiteOpenHelper{
+
+    private static final String DATABASE_NAME = "mycontacts.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String CREATE_TABLE_CONTACT =
+            "create table contact (_id integer primary key autoincrement,"
+            + "editTextFirstID text not null, editTextLastID text not null, editTextStrAddress text, "
+            + "editTextCityAddress text, editTextStateAddress text, editTextZipAddress text, "
+            + "editTextPhone text"
+            + "editTextEmail text, birthViewID text, editTextCountryAddress text);";
+
+    public ContactDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE_CONTACT);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(ContactDBHelper.class.getName(),
+                "Upgrading database from version" + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS contact");
+        onCreate(db);
+    }
 }
