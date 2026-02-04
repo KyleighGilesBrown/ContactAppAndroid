adcf4b970b85a57f24fe4f679d1ad61964cb4c07
kyleighgiles-brown <kyleighgilesbrown@gmail.com>
2026-02-03 23:25:31 -0500

    will resume tomorrow. 3 more things todo.

---
+++ app/src/main/java/com/example/contactapplication/Contact.java
@@ -0,0 +1,112 @@
+package com.example.contactapplication;
+import java.util.Calendar;
+
+public class Contact {
+    private int contactID;
+    private String editTextEmail;
+    private String editTextStrAddress;
+    private String editTextCityAddress;
+    private String editTextZipAddress;
+
+    private String editTextStateAddress;
+    private String editTextCountryAddress;
+    private String editTextPhone;
+    private String editTextFirstID;
+
+    private String editTextLastID;
+
+    private Calendar birth;
+
+    public Contact() {
+        contactID = -1;
+        birth = Calendar.getInstance();
+    }
+
+    public int getContactID() {
+        return contactID;
+    }
+
+    public void setContactID(int i) {
+        contactID = i;
+    }
+
+    public String getEditTextEmail() {
+        return editTextEmail;
+    }
+
+    public void setEditTextEmail(String editTextEmail) {
+        this.editTextEmail = editTextEmail;
+    }
+
+    public String getEditTextStrAddress() {
+        return editTextStrAddress;
+    }
+
+    public void setEditTextStrAddress(String editTextStrAddress) {
+        this.editTextStrAddress = editTextStrAddress;
+    }
+
+    public String getEditTextCityAddress() {
+        return editTextCityAddress;
+    }
+
+    public void setEditTextCityAddress(String editTextCityAddress) {
+        this.editTextCityAddress = editTextCityAddress;
+    }
+
+    public String getEditTextZipAddress() {
+        return editTextZipAddress;
+    }
+
+    public void setEditTextZipAddress(String editTextZipAddress) {
+        this.editTextZipAddress = editTextZipAddress;
+    }
+
+    public String getEditTextStateAddress() {
+        return editTextStateAddress;
+    }
+
+    public void setEditTextStateAddress(String editTextStateAddress) {
+        this.editTextStateAddress = editTextStateAddress;
+    }
+
+    public String getEditTextCountryAddress() {
+        return editTextCountryAddress;
+    }
+
+    public void setEditTextCountryAddress(String editTextCountryAddress) {
+        this.editTextCountryAddress = editTextCountryAddress;
+    }
+
+    public String getEditTextPhone() {
+        return editTextPhone;
+    }
+
+    public void setEditTextPhone(String editTextPhone) {
+        this.editTextPhone = editTextPhone;
+    }
+
+    public String getEditTextFirstID() {
+        return editTextFirstID;
+    }
+
+    public void setEditTextFirstID(String editTextFirstID) {
+        this.editTextFirstID = editTextFirstID;
+    }
+
+    public String getEditTextLastID() {
+        return editTextLastID;
+    }
+
+    public void setEditTextLastID(String editTextLastID) {
+        this.editTextLastID = editTextLastID;
+    }
+
+    public Calendar getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Calendar birth) {
+        this.birth = birth;
+    }
+}
---
+++ app/src/main/java/com/example/contactapplication/ContactDataSource.java
@@ -0,0 +1,73 @@
+package com.example.contactapplication;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.sql.SQLException;
+
+public class ContactDataSource {
+    private SQLiteDatabase database;
+    private ContactDBHelper dbHelper;
+
+    public ContactDataSource (Context context) {
+        dbHelper = new ContactDBHelper(context);
+
+    }
+    public void open() throws SQLException {
+        database = dbHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
+
+    public boolean insertContact(Contact c) {
+        boolean didSucceed = false;
+        try {
+            ContentValues initialValues = new ContentValues();
+            initialValues.put("editTextEmail", c.getEditTextEmail());
+            initialValues.put("editTextStrAddress", c.getEditTextStrAddress());
+            initialValues.put("editTextCityAddress", c.getEditTextCityAddress());
+            initialValues.put("editTextZipAddress", c.getEditTextZipAddress());
+            initialValues.put("editTextStateAddress", c.getEditTextStateAddress());
+            initialValues.put("editTextCountryAddress", c.getEditTextCountryAddress());
+            initialValues.put("editTextPhone", c.getEditTextPhone());
+            initialValues.put("editTextFirstID", c.getEditTextFirstID());
+            initialValues.put("editTextLastID", c.getEditTextLastID());
+            initialValues.put("birth", String.valueOf(c.getBirth().getTimeInMillis()));
+
+            didSucceed = database.insert("contact", null, initialValues) > 0;
+        }
+        catch (Exception e) {
+
+        }
+        return didSucceed;
+
+    }
+    public boolean updateContact(Contact c) {
+        boolean didSucceed = false;
+        try {
+            Long rowId = (long) c.getContactID();
+            ContentValues updateValues = new ContentValues();
+            updateValues.put("editTextEmail", c.getEditTextEmail());
+            updateValues.put("editTextStrAddress", c.getEditTextStrAddress());
+            updateValues.put("editTextCityAddress", c.getEditTextCityAddress());
+            updateValues.put("editTextZipAddress", c.getEditTextZipAddress());
+            updateValues.put("editTextStateAddress", c.getEditTextStateAddress());
+            updateValues.put("editTextCountryAddress", c.getEditTextCountryAddress());
+            updateValues.put("editTextPhone", c.getEditTextPhone());
+            updateValues.put("editTextFirstID", c.getEditTextFirstID());
+            updateValues.put("editTextLastID", c.getEditTextLastID());
+            updateValues.put("birth", String.valueOf(c.getBirth().getTimeInMillis()));
+
+            didSucceed = database.insert("contact", null, updateValues) > 0;
+        }
+        catch (Exception e) {
+
+        }
+        return didSucceed;
+
+    }
+    }
+
